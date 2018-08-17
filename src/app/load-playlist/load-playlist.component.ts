b3f451f216328b7ea352b0da6c1718f4aac4b186
pontius G <pontius@pnts.co>
2018-08-18 00:17:50 +0300

    basic file upload

--- src/app/load-playlist/load-playlist.component.ts
+++ src/app/load-playlist/load-playlist.component.ts
@@ -9,4 +9,12 @@ export class LoadPlaylistComponent implements OnInit {
   playlistInputHint: string = "Waiting for file...";
   constructor() {}
   ngOnInit() {}
+  playlistUpld=(ev:DragEvent,name:String)=>{
+    ev.stopPropagation();
+    ev.preventDefault();
+    let file=(ev.type==='drop')? ev.dataTransfer.files[0]:(<HTMLInputElement>ev.target).files[0];
+    this.playlistInputHint=file.name;
+    let listname=name || file.name;
+    console.log(listname);
+  }
 }
